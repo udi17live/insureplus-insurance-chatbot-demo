0e6eb7f1d52354abd682f6bbec7cb01b540638ed
Uditha Mahindarathna <melan.udi@gmail.com>
2026-06-08 03:12:31 +0530

    feat: change storage_uri in limiter to use in-memory storage

--- backend/app/core/limiter.py
+++ backend/app/core/limiter.py
@@ -1,9 +1,8 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from app.core.config import settings
 
 limiter = Limiter(
     key_func=get_remote_address,
-    storage_uri=settings.redis_url,
+    storage_uri="memory://",
     default_limits=[],
 )
